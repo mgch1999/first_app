2d930fb5e031135b88c104453095ce4a9b7b49b6
111175040+mgch1999@users.noreply.github.com <beatlove308@gmail.com>
2022-12-14 17:24:49 +0900

    aa

--- test.py
+++ test.py
@@ -36,96 +36,137 @@ ward = ("千代田区", "中央区", "港区", "新宿区", "文京区", "台東
 
 ymin, ymax = 0, 1500
 
-area1 = st.sidebar.selectbox("エリア選択", ward)
-area2 = st.sidebar.selectbox("比較エリア選択", ward)
-madori = st.sidebar.selectbox("間取りタイプ",  ("ワンルーム", "1K", "1LDK"))
+query = f"""
+WITH data_with_ku AS (
+SELECT
+*,
+CASE 
+    WHEN address LIKE '%千代田区%' THEN '千代田区' 
+    WHEN address LIKE '%中央区%' THEN '中央区' 
+    WHEN address LIKE '%港区%' THEN '港区' 
+    WHEN address LIKE '%新宿区%' THEN '新宿区' 
+    WHEN address LIKE '%文京区%' THEN '文京区' 
+    WHEN address LIKE '%台東区%' THEN '台東区' 
+    WHEN address LIKE '%墨田区%' THEN '墨田区' 
+    WHEN address LIKE '%江東区%' THEN '江東区' 
+    WHEN address LIKE '%品川区%' THEN '品川区' 
+    WHEN address LIKE '%目黒区%' THEN '目黒区' 
+    WHEN address LIKE '%大田区%' THEN '大田区' 
+    WHEN address LIKE '%世田谷区%' THEN '世田谷区' 
+    WHEN address LIKE '%渋谷区%' THEN '渋谷区' 
+    WHEN address LIKE '%中野区%' THEN '中野区' 
+    WHEN address LIKE '%杉並区%' THEN '杉並区' 
+    WHEN address LIKE '%豊島区%' THEN '豊島区' 
+    WHEN address LIKE '%北区%' THEN '北区' 
+    WHEN address LIKE '%荒川区%' THEN '荒川区' 
+    WHEN address LIKE '%板橋区%' THEN '板橋区' 
+    WHEN address LIKE '%練馬区%' THEN '練馬区' 
+    WHEN address LIKE '%足立区%' THEN '足立区' 
+    WHEN address LIKE '%葛飾区%' THEN '葛飾区' 
+    WHEN address LIKE '%江戸川区%' THEN '江戸川区' 
+END AS ku
+FROM
+    `prediction-rent-price.dataset1.tokyo_1k`
+)
+SELECT
+*
+FROM
+data_with_ku;"""
+         
+data = client.query(query).to_dataflame()
+st.dataframe(data)
 
 
-def scatter(madori):
+# area1 = st.selectbox("エリア選択", ward)
+# area2 = st.selectbox("比較エリア選択", ward)
+# madori = st.selectbox("間取りタイプ",  ("ワンルーム", "1K", "1LDK"))
 
-    if area1 == area2:
-        query = f"""
-        WITH data_with_ku AS (
-        SELECT
-        *,
-        CASE 
-            WHEN address LIKE '%{area1}%' THEN '{area1}' 
-        END AS ku
-        FROM
-            `prediction-rent-price.dataset1.{madori}`
-        )
-        SELECT
-        *
-        FROM
-        data_with_ku;"""
 
-        data = client.query(query).to_dataframe()
-        df = pd.DataFrame(data)
-        df1 = df.loc[(df["ku"]==area1)]
+# def scatter(madori):
+
+#     if area1 == area2:
+#         query = f"""
+#         WITH data_with_ku AS (
+#         SELECT
+#         *,
+#         CASE 
+#             WHEN address LIKE '%{area1}%' THEN '{area1}' 
+#         END AS ku
+#         FROM
+#             `prediction-rent-price.dataset1.{madori}`
+#         )
+#         SELECT
+#         *
+#         FROM
+#         data_with_ku;"""
+
+#         data = client.query(query).to_dataframe()
+#         df = pd.DataFrame(data)
+#         df1 = df.loc[(df["ku"]==area1)]
         
-        fig, ax = plt.subplots()
-        ax.scatter(df1["sizes"], df1["prices"], alpha=0.4, color="dodgerblue",s=10, label=area1)
-        plt.xlabel("面積(m2)")
-        plt.ylabel("家賃(万円)")
-        plt.legend()
-        st.pyplot(fig)
+#         fig, ax = plt.subplots()
+#         ax.scatter(df1["sizes"], df1["prices"], alpha=0.4, color="dodgerblue",s=10, label=area1)
+#         plt.xlabel("面積(m2)")
+#         plt.ylabel("家賃(万円)")
+#         plt.legend()
+#         st.pyplot(fig)
         
-    else:
-        query = f"""
-        WITH data_with_ku AS (
-        SELECT
-        *,
-        CASE 
-            WHEN address LIKE '%{area1}%' THEN '{area1}' 
-            WHEN address LIKE '%{area2}%' THEN '{area2}'
-        END AS ku
-        FROM
-            `prediction-rent-price.dataset1.{madori}`
-        )
-        SELECT
-        *
-        FROM
-        data_with_ku;"""
+#     else:
+#         query = f"""
+#         WITH data_with_ku AS (
+#         SELECT
+#         *,
+#         CASE 
+#             WHEN address LIKE '%{area1}%' THEN '{area1}' 
+#             WHEN address LIKE '%{area2}%' THEN '{area2}'
+#         END AS ku
+#         FROM
+#             `prediction-rent-price.dataset1.{madori}`
+#         )
+#         SELECT
+#         *
+#         FROM
+#         data_with_ku;"""
 
-        data = client.query(query).to_dataframe()
-        df = pd.DataFrame(data)
-        df1 = df.loc[(df["ku"]==area1)]
-        df2 = df.loc[(df["ku"]==area2)]
+#         data = client.query(query).to_dataframe()
+#         df = pd.DataFrame(data)
+#         df1 = df.loc[(df["ku"]==area1)]
+#         df2 = df.loc[(df["ku"]==area2)]
 
-        fig, ax = plt.subplots()
-        ax.scatter(df1["sizes"], df1["prices"], alpha=0.4, color="dodgerblue",s=10, label=area1)
-        ax.scatter(df2["sizes"], df2["prices"], alpha=0.1, color="orange",s=10, label=area2)
-        plt.xlabel("面積(m2)")
-        plt.ylabel("家賃(万円)")
-        plt.legend()
-        st.pyplot(fig)
-class Select():
-    def __init__(self, area1, area2, madori):
-        self.area1 = area1
-        self.area2 = area2
-        self.madori = madori
+#         fig, ax = plt.subplots()
+#         ax.scatter(df1["sizes"], df1["prices"], alpha=0.4, color="dodgerblue",s=10, label=area1)
+#         ax.scatter(df2["sizes"], df2["prices"], alpha=0.1, color="orange",s=10, label=area2)
+#         plt.xlabel("面積(m2)")
+#         plt.ylabel("家賃(万円)")
+#         plt.legend()
+#         st.pyplot(fig)
+# class Select():
+#     def __init__(self, area1, area2, madori):
+#         self.area1 = area1
+#         self.area2 = area2
+#         self.madori = madori
 
-    def select_scatter(self): 
-        if self.area1 == area2:
-            self.area1 = area1 
-            self.area2 = area2  
-            if self.madori == "ワンルーム":
-                scatter("tokyo_1r")
-            elif self.madori == "1K":
-                scatter("tokyo_1k")
-            else:
-                scatter("tokyo_1ldk")
-        else:
-            self.area1 = area1
-            if self.madori == "ワンルーム":
-                scatter("tokyo_1r")
-            elif self.madori == "1K":
-                scatter("tokyo_1k")
-            else:
-                scatter("tokyo_1ldk")
+#     def select_scatter(self): 
+#         if self.area1 == area2:
+#             self.area1 = area1 
+#             self.area2 = area2  
+#             if self.madori == "ワンルーム":
+#                 scatter("tokyo_1r")
+#             elif self.madori == "1K":
+#                 scatter("tokyo_1k")
+#             else:
+#                 scatter("tokyo_1ldk")
+#         else:
+#             self.area1 = area1
+#             if self.madori == "ワンルーム":
+#                 scatter("tokyo_1r")
+#             elif self.madori == "1K":
+#                 scatter("tokyo_1k")
+#             else:
+#                 scatter("tokyo_1ldk")
 
-scatter_plot = Select(area1, area2, madori)
-scatter_plot.select_scatter()
+# scatter_plot = Select(area1, area2, madori)
+# scatter_plot.select_scatter()
 
 # fig, ax = plt.subplots()
 # plt.hist(df1["prices"],alpha=0.3, bins=50, color="dodgerblue", label=area1)
