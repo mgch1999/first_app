27d38914b267cba5104d09eb59430ce3d6b01677
111175040+mgch1999@users.noreply.github.com <beatlove308@gmail.com>
2022-12-14 17:58:50 +0900

    a

--- test.py
+++ test.py
@@ -75,10 +75,10 @@ data_with_ku
 GROUP BY 
 ku;"""
          
+pd.options.display.float_format = "{:.2g}".format
 data = client.query(query).to_dataframe()
 df = pd.DataFrame(data)
 df = df.sort_values("f0_", ascending=False)
-st.dataframe(df)
 fig, ax = plt.subplots()
 p = ax.bar(df["ku"], height=df["f0_"], color="dodgerblue")
 ax.bar_label(p)
