738c3b0aacc19190228c59d382ec52ab60c8a3a5
111175040+mgch1999@users.noreply.github.com <beatlove308@gmail.com>
2022-12-18 18:28:31 +0900

    a

--- test.py
+++ test.py
@@ -178,19 +178,17 @@ def scatter():
 
 def analysis_23(madori):
 
-    st.dataframe(df)
-    
-    
-    # left, right = st.columns(2)
-    # with left:
-    #     df = pd.pivot_table(df, index="ku", values="prices")
-    #     df = df.sort_values("prices", ascending=False)
-    #     st.table(df.style.format('{:.1f}'))
-    # with right:
-    #     fig, ax = plt.subplots()
-    #     ax.bar(df.index, height=df["prices"], color="dodgerblue")
-    #     plt.xticks(rotation=50)
-    #     st.pyplot(fig)
+    df = df
+    left, right = st.columns(2)
+    with left:
+        df = pd.pivot_table(df, index="ku", values="prices")
+        df = df.sort_values("prices", ascending=False)
+        st.table(df.style.format('{:.1f}'))
+    with right:
+        fig, ax = plt.subplots()
+        ax.bar(df.index, height=df["prices"], color="dodgerblue")
+        plt.xticks(rotation=50)
+        st.pyplot(fig)
 
     
     
