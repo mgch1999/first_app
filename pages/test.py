3cdd5f1cc1df3e930c1264afb0019e2292610e5a
111175040+mgch1999@users.noreply.github.com <beatlove308@gmail.com>
2022-12-23 22:20:06 +0900

    a

--- pages/test.py
+++ pages/test.py
@@ -331,7 +331,7 @@ def analysis4():
         for i in range(len(freq.index)):
             bar_list = ax.barh(freq.columns, freq.iloc[i], color=colors[i], left=left_data, height=0.5)
             left_data += freq.iloc[i]
-        ax.legend(label, loc='upper left', bbox_to_anchor=(1, 1), fontsize=20)
+        ax.legend(label, loc='upper left', bbox_to_anchor=(1, 1))
         plt.xlim([0, 1])
         st.pyplot(fig)
         
