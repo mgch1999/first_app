d300c845f99384a1c6624365139f4377606c268f
111175040+mgch1999@users.noreply.github.com <beatlove308@gmail.com>
2022-12-23 11:54:07 +0900

    a

--- pages/test.py
+++ pages/test.py
@@ -304,7 +304,7 @@ def analysis4():
         plt.legend()
         st.pyplot(fig)
     with left:
-        bins = np.linspace(0, 20, 2.5)
+        bins = np.linspace(0, 20, 2)
         freq = df_ward1[hennsuu1].value_counts(bins=bins, sort=False)
         dist = pd.DataFrame({"度数":freq}, index=freq.index)
         st.dataframe(dist)
