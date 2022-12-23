c308f1b6f9875c59f9dc5661bc008d5cb35cfd4a
111175040+mgch1999@users.noreply.github.com <beatlove308@gmail.com>
2022-12-23 12:01:15 +0900

    a

--- pages/test.py
+++ pages/test.py
@@ -304,7 +304,7 @@ def analysis4():
         plt.legend()
         st.pyplot(fig)
     with left:
-        bins = np.arange(0, 20, 2)
+        bins = np.arange(0, 25, 2.5)
         freq = df_ward1[hennsuu1].value_counts(bins=bins, sort=False)
         st.dataframe(freq)
 
