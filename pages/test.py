deedf61060f4932aeef3013d06b647e6abbfbfba
111175040+mgch1999@users.noreply.github.com <beatlove308@gmail.com>
2022-12-24 15:18:43 +0900

    a

--- pages/test.py
+++ pages/test.py
@@ -341,7 +341,7 @@ def analysis4():
     table = pd.DataFrame({area1:[df_ward1["prices"].mean(), df_ward1["sizes"].mean(), df_ward1["yearss"].mean(), df_ward1["accesses"].mean()],
                           area2:[df_ward2["prices"].mean(), df_ward2["sizes"].mean(), df_ward2["yearss"].mean(), df_ward2["accesses"].mean()]},
                           index=["平均家賃(万円)", "面積(m2)", "築年数", "アクセス(分)"])
-    st.dataframe(table.style.format('{:.1f}'))
+    st.table(table.style.format('{:.1f}'))
 
 
 
