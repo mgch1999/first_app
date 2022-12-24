1e91ac46304393cc84316708056d957328a3c02d
111175040+mgch1999@users.noreply.github.com <beatlove308@gmail.com>
2022-12-24 15:11:46 +0900

    a

--- pages/test.py
+++ pages/test.py
@@ -339,8 +339,8 @@ def analysis4():
         st.pyplot(fig)
     
     table = pd.DataFrame({f"area1":[avg[df_ward1["prices"]], avg[df_ward1["sizes"]], avg[df_ward1["yearss"]], avg[df_ward1["accesses"]]],
-                          f"area2":[avg[df_ward2["prices"]], avg[df_ward2["sizes"]], avg[df_ward2["yearss"]], avg[df_ward2["accesses"]]]},
-                          index=["平均家賃(万円)", "面積(m2)", "築年数", "アクセス(分)"])
+                          f"area2":[avg[df_ward2["prices"]], avg[df_ward2["sizes"]], avg[df_ward2["yearss"]], avg[df_ward2["accesses"]]]})
+                        #   index=["平均家賃(万円)", "面積(m2)", "築年数", "アクセス(分)"])
     st.table(table)
 
 
