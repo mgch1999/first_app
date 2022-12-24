ad3346510bc2a38302ba3bd882dc4ea33c1a6078
111175040+mgch1999@users.noreply.github.com <beatlove308@gmail.com>
2022-12-24 15:14:34 +0900

    a

--- pages/test.py
+++ pages/test.py
@@ -338,9 +338,9 @@ def analysis4():
         plt.xlim([0, 1])
         st.pyplot(fig)
     
-    table = pd.DataFrame({f"area1":[avg[df_ward1["prices"]], avg[df_ward1["sizes"]], avg[df_ward1["yearss"]], avg[df_ward1["accesses"]]],
-                          f"area2":[avg[df_ward2["prices"]], avg[df_ward2["sizes"]], avg[df_ward2["yearss"]], avg[df_ward2["accesses"]]]})
-                        #   index=["平均家賃(万円)", "面積(m2)", "築年数", "アクセス(分)"])
+    table = pd.DataFrame({f"area1":[df_ward1["prices"].mean(), df_ward1["sizes"].mean(), df_ward1["yearss"].mean(), df_ward1["accesses"].mean()],
+                          f"area2":[df_ward2["prices"].mean(), df_ward2["sizes"].mean(), df_ward2["yearss"].mean(), df_ward2["accesses"].mean()]},
+                          index=["平均家賃(万円)", "面積(m2)", "築年数", "アクセス(分)"])
     st.table(table)
 
 
